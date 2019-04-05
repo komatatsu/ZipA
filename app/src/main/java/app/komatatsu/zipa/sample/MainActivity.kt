4fbb10b3758ec52464cd9797596704e86818516b
こまたつ <ceo@komasec.com>
2019-04-05 14:16:46 +0900

    サンプルアプリを作成

--- app/src/main/java/app/komatatsu/zipa/sample/MainActivity.kt
+++ app/src/main/java/app/komatatsu/zipa/sample/MainActivity.kt
@@ -1,12 +1,21 @@
 package app.komatatsu.zipa.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        search.setOnClickListener {
+            Log.v(this.javaClass.simpleName, "${editText.text}")
+            // TODO call ZipA.search
+            result.text = "結果"
+        }
+
     }
 }
